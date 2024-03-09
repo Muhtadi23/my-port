922c73c6a2ad4c757fa57f6214e5c68012751bc9
Muhtadi23 <miranmuhtadi121@gmail.com>
2024-03-10 00:44:58 +0600

    nav on progress

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+
+  return (
+    <>
+      <div className="bg-black max-w-[1440px] mx-auto">
+        <Navbar></Navbar>
+      </div>
+
+    </>
+  )
+}
+
+export default App
